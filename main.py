fb4608c63ac99d02e3d099b908d494596b483fdd
yu-efremov <58818457+yu-efremov@users.noreply.github.com>
2024-08-07 11:35:33 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from background import keep_alive #импорт функции для поддержки работоспособности
+import pip
+pip.main(['install', 'pytelegrambotapi'])
+import telebot
+import time
+
+bot = telebot.TeleBot(os.environ['telegram_bot_API_token'])
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_message(message):
+  bot.send_message(message.from_user.id, message.text)
+
+
+# echo-функция, которая отвечает на любое текстовое сообщение таким же текстом
+
+keep_alive()  #запускаем flask-сервер в отдельном потоке. Подробнее ниже...
+bot.polling(non_stop=True, interval=0)  #запуск бота
