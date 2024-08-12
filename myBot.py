3af53148748c054c968ea1a6aca11ede57f3bd5a
yu-efremov <58818457+yu-efremov@users.noreply.github.com>
2024-08-12 16:53:02 +0300

    Create myBot.py

---
+++ myBot.py
@@ -0,0 +1,14 @@
+import os
+import telebot
+import threading
+
+bot = telebot.TeleBot(os.environ['telegram_bot_API_token'])
+
+def bot_check():
+    return bot.get_me()
+
+def bot_runner():
+    bot.infinity_polling(none_stop=True)
+
+t = threading.Thread(target=bot_runner)
+t.start()
