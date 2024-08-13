defae5a64662c9b113d30f21730c99ef461e46aa
yu-efremov <58818457+yu-efremov@users.noreply.github.com>
2024-08-13 10:26:21 +0300

    Update myBot.py

--- myBot.py
+++ myBot.py
@@ -1,14 +1,14 @@
-import os
-import telebot
-import threading
+# import os
+# import telebot
+# import threading
 
-bot = telebot.TeleBot(os.environ['telegram_bot_API_token'])
+# bot = telebot.TeleBot(os.environ['telegram_bot_API_token'])
 
-def bot_check():
-    return bot.get_me()
+# def bot_check():
+#     return bot.get_me()
 
-def bot_runner():
-    bot.infinity_polling(none_stop=True)
+# def bot_runner():
+#     bot.infinity_polling(none_stop=True)
 
-t = threading.Thread(target=bot_runner)
-t.start()
+# t = threading.Thread(target=bot_runner)
+# t.start()
