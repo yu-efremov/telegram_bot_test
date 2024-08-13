2d2be407fb11eab85319afe1d2a6085731786dfc
yu-efremov <58818457+yu-efremov@users.noreply.github.com>
2024-08-13 10:25:24 +0300

    Update background.py

--- background.py
+++ background.py
@@ -3,31 +3,24 @@ from flask import request
 from threading import Thread
 import time
 import requests
-import myBot # бот в отдельном thread
-
 
 app = Flask('')
 
+
 @app.route('/')
 def home():
-  if myBot.bot_check():
-      return "I'm alive and bot is checked"
-  else:
-      print("Problems with bot")
+  return "I'm alive"
+
 
 @app.route("/favicon.ico")
 def favicon():
-    return url_for('static', filename='data:,')
+  return url_for('static', filename='data:,')
+
 
 def run():
   app.run(host='0.0.0.0', port=80)
 
+
 def keep_alive():
   t = Thread(target=run)
   t.start()
-
-def home():
-  if myBot.bot_check():
-      return "I'm alive and bot is checked"
-  else:
-      print("Problems with bot")
